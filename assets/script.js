7684022c7089a891e367e3a1a3fb837ed3eb363b
SanyamSamS <sanyam.edu@gmail.com>
2023-11-14 00:09:54 -0500

    retrieve search bar value on click

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,31 @@ var searchBtn = document.getElementById('search-btn');
 var currentWeatherContainer = document.getElementById('current-weather');
 var forecastContainer = document.getElementById('forecast');
 var searchHistoryContainer = document.getElementById('search-history-list');
+
+// Search bar
+searchBtn.addEventListener('click', function () {
+    // Retrieve input from the search bar value
+    var cityName = searchInput.value.trim();
+
+    if (cityName !== '') {
+        getWeather(cityName);
+    }
+    console.log(cityName);
+});
+
+
+
+// Use input value to get lon and lat for api call
+function getWeather(cityName){
+    
+};
+
+// Populate the current weather container
+
+// Populate the 5 day forecast container 
+
+// Add input values to search history container
+
+// Store searched cities to local storage
+
+// Update page to retrieve local storage to display on search history
